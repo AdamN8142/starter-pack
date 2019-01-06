c63f43aeada32d857fe880089a21b324978fb930
Adam Niedzwiecki <adamn8142@gmail.com>
2019-01-05 17:55:02 -0700

    Decorate individual card

--- src/Card.js
+++ src/Card.js
@@ -7,11 +7,11 @@ class Card extends Component {
     }
     render() {
         return(
-          <div>
+          <div className="individual-question">
             <p>{this.props.question}</p>
             {
                 this.props.answers.map((answer) => {
-                    return (<button>{answer}</button>)
+                    return (<button className="answer-buttons">{answer}</button>)
                 })
             }
         
