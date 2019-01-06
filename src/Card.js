5082ade62dc86b22ce8c116adff1d238f0203cf0
Adam Niedzwiecki <adamn8142@gmail.com>
2019-01-05 17:09:26 -0700

    Filter through categories

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,20 @@
 import React, { Componenet } from 'react';
 
 
-class CardContainer extends Component {
-    constructor() {
-        super();
+class Card extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+          <div>
+            <p>{props.question}</p>
+            <button>{props.category.possibleAnswer}</button>
+            <button>{props.category.possibleAnswer}</button>
+            <button>{props.category.possibleAnswer}</button>
+          </div>   
+        )
     }
 }
+
+export default Card;
--- src/CardContainer.js
+++ src/CardContainer.js
@@ -1,9 +1,23 @@
-import React, { Componenet } from 'react';
+import React, { Component } from 'react';
 
 
 class CardContainer extends Component {
-    constructor(){
-        super()
+    constructor(props){
+      super(props);
+      this.state = {
+        category: this.props.grabCategory
+        }
     }
 
+
+
+    render() {
+        return(
+            <div>
+                {/* <Card cardInfo={this.props.category}/> */}
+            </div>
+        )
+    }
 }
+
+export default CardContainer;
